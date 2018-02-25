384c318bc7d2b3ec0480bd9b0384d15b0502246f
Xiao Fan <xiao.h.fan@gmail.com>
2018-02-24 23:56:16 -0800

    status and list-timers

--- systemd_aliases.sh
+++ systemd_aliases.sh
@@ -1,5 +1,7 @@
 alias sc=systemctl
 alias scu='systemctl --user'
+alias scust='systemctl --user status'
+alias scult='systemctl --user list-timers'
 alias scu-new='systemctl --user edit --full --force'
 alias jc=journalctl
 alias jcu='journalctl --user'
