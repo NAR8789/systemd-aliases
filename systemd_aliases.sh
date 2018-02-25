266dcf176894aa1416d41ee6812718d9ff7304bc
Xiao Fan <xiao.h.fan@gmail.com>
2018-02-25 07:36:41 -0800

    include psc

--- systemd_aliases.sh
+++ systemd_aliases.sh
@@ -5,3 +5,6 @@ alias scult='systemctl --user list-timers'
 alias scu-new='systemctl --user edit --full --force'
 alias jc=journalctl
 alias jcu='journalctl --user'
+
+alias psc='ps xawf -eo pid,user,cgroup,args'
+# from http://0pointer.de/blog/projects/systemd-for-admins-2.html
